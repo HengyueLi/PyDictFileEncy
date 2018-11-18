984c80ff58ed3ca7ef68b37a2cfc30906513ebf4
Unknown <305069590@qq.com>
2018-11-18 16:33:52 +0900

    update script upload

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup
 
+from setuptools import setup
 setup(
     name='pydictfileency',
-    version='2018.11.18',
+    version='2018.11.181',
     author='Hengyue Li',
     author_email='hengyue.li@hengyue.li',
     packages=['pydictfileency', 'pydictfileency.test'],
@@ -11,5 +11,5 @@ setup(
     description='A simply API to encrypt the python dict and save it into a file.',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    install_requires=[ jc for jc in open('requirements.txt').read().split('\n') if len(jc)>1  ] ,
+    install_requires=['certifi==2018.10.15', 'chardet==3.0.4', 'idna==2.7', 'Naked==0.1.31', 'pycrypto==2.6.1', 'PyYAML==3.13', 'requests==2.20.1', 'shellescape==3.4.1', 'urllib3==1.24.1'],
 )
--- upload.py
+++ upload.py
@@ -6,6 +6,33 @@ filepath = os.path.realpath(__file__)
 projpath = os.path.dirname(filepath)
 dist = os.path.join(projpath,'dist')
 
+
+
+
+install_requires = [ jc for jc in open('requirements.txt').read().split('\n') if len(jc)>1  ]
+
+setupcontext = '''
+from setuptools import setup
+setup(
+    name='pydictfileency',
+    version='2018.11.181',
+    author='Hengyue Li',
+    author_email='hengyue.li@hengyue.li',
+    packages=['pydictfileency', 'pydictfileency.test'],
+    url='https://github.com/HengyueLi/PyDictFileEncy',
+    license='LICENSE.md',
+    description='A simply API to encrypt the python dict and save it into a file.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    install_requires={},
+)
+'''.format(install_requires)
+
+
+
 os.system('rm -rf {}/*'.format(dist))
+os.system('rm -rf setup.py')
+with open('setup.py','w') as f:
+    f.write(setupcontext)
 os.system('{} setup.py sdist'.format(python))
 os.system('twine upload dist/*')
