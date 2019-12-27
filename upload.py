74ec09ff9e6d6818e2a00f54efe22c0ba6439f6b
h. l <305069590@qq.com>
2019-12-27 10:01:45 +0800

    adopt urllib3 update

--- upload.py
+++ upload.py
@@ -1,32 +1,50 @@
+#!/usr/bin/env python3
+import sys,os,datetime
 
-import sys,os
+
+#--------------
+name    = 'pydictfileency'
+scripts = []
+#--------------
+
+
+
 
 python   = sys.executable
 filepath = os.path.realpath(__file__)
 projpath = os.path.dirname(filepath)
 dist = os.path.join(projpath,'dist')
+passDir = os.path.join(os.environ['HOME'],'Dropbox/AutoPassword')
+sys.path.insert(0, passDir)
+import password as pw
 
 
-
-
-install_requires = [ jc for jc in open('requirements.txt').read().split('\n') if len(jc)>1  ]
 
 setupcontext = '''
-from setuptools import setup
-setup(
-    name='pydictfileency',
-    version='2018.11.181',
+import setuptools
+#from setuptools import setup
+
+setuptools.setup(
+    name='{name}',
+    scripts={scripts} ,
+    version='{version}',
     author='Hengyue Li',
     author_email='hengyue.li@hengyue.li',
-    packages=['pydictfileency', 'pydictfileency.test'],
-    url='https://github.com/HengyueLi/PyDictFileEncy',
+    packages=setuptools.find_packages(),
     license='LICENSE.md',
-    description='A simply API to encrypt the python dict and save it into a file.',
-    long_description=open('README.md').read(),
+    description='A tool based on paramiko used to connect to remote server.',
+    long_description=open('README.md',encoding="utf8").read(),
     long_description_content_type="text/markdown",
-    install_requires={},
+    install_requires={install_requires},
+    python_requires='>=3.5',
+    url = "https://github.com/HengyueLi/easycon",
 )
-'''.format(install_requires)
+'''.format(name    = name ,
+           scripts = str(scripts),
+           version = datetime.datetime.now().strftime("%Y.%m.%d.%H%M") ,
+           install_requires = [ i for i in open('requirements.txt').read().split('\n') if len(i)>1  ] )
+
+
 
 
 
@@ -35,4 +53,11 @@ os.system('rm -rf setup.py')
 with open('setup.py','w') as f:
     f.write(setupcontext)
 os.system('{} setup.py sdist'.format(python))
-os.system('twine upload dist/*')
+#---------------------------------------
+# upload with username&password
+command = "twine upload dist/* -u {username} -p {password}".format(
+                username = pw.GetAutoPasswd('pip','username').get(),
+                password = pw.GetAutoPasswd('pip','password').get() )
+os.system(command)
+#---------------------------------------
+os.system('rm -rf dist setup.py *egg-info*')
